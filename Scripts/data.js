5579e1e55ccef45c28ea57012d919e6634227f61
Eliana <51841108+Cheese-Curd@users.noreply.github.com>
2025-10-10 15:18:24 -0400

    Part 2

--- Scripts/data.js
+++ Scripts/data.js
@@ -1,20 +1,18 @@
 // Subjective submit logic and objective logic
 document.getElementById('submitSubjective').addEventListener('click', () => {
-
-
-	const ObjectiveMatchNumber = document.getElementById("ObjectiveMatchNumber").value; // Added to get Objective Match Number 
+	const ObjectiveMatchNumber  = document.getElementById("ObjectiveMatchNumber").value; // Added to get Objective Match Number 
 	const ScoutersNameObjective = document.getElementById("ScoutersNameObjective").value;// Added to get Scouters Name
-	const ObjectiveTeamNumber = document.getElementById("ObjectiveTeamNumber").value;// Added to get Objective Team Number
+	const ObjectiveTeamNumber   = document.getElementById("ObjectiveTeamNumber").value;// Added to get Objective Team Number
 
-	const teamNumber = document.getElementById('teamNumber').value;// Get Team Number
+	const teamNumber  = document.getElementById('teamNumber').value;// Get Team Number
 	const teamNumber2 = document.getElementById('teamNumber2').value;// Get the second Team Number
 	const teamNumber3 = document.getElementById('teamNumber3').value;// get the third Team Number
 	const matchNumber = document.getElementById('matchNumber').value;// get the match number
 
 	const strategyNotes = document.getElementById('strategyNotes').value;// get the strategy notes from subjective
-	const driverSkill = document.getElementById('driverSkill').value;// get the driver skill from sunjective
+	const driverSkill   = document.getElementById('driverSkill').value;// get the driver skill from sunjective
 	const communication = document.getElementById('communication').value;//get the communication from subjective
-	const reliability = document.getElementById('reliability').value;// get the reliability from subjective
+	const reliability   = document.getElementById('reliability').value;// get the reliability from subjective
 
 
 
@@ -99,7 +97,8 @@ document.getElementById('submitSubjective').addEventListener('click', () => {
 	alert("Subjective data saved!");// alert user
 	subjectiveCompleted = true;// set subjective completed to true
 
-	console.log("Updating QRCode!")// log to console for testing
+	console.log("Updating QRCodes!")// log to console for testing
+
 	// Clear previous QR code
 	const objectiveContainer  = document.getElementById('objQRCode');
 	const subjectiveContainer = document.getElementById('subQRCode');
@@ -125,8 +124,6 @@ document.getElementById('submitSubjective').addEventListener('click', () => {
 		colorLight: "#ffffff",
 		correctLevel: QRCode.CorrectLevel.L
 	});
-
-
 
 	document.getElementById("outputData").style.display = "block";// display the output data container
 });
